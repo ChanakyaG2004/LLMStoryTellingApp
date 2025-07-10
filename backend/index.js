0a88801b0011d8622fb5f7b8fb51a7f69455f218
ChanakyaG2004 <126143662+Kya2004@users.noreply.github.com>
2025-07-10 01:08:04 -0700

    lastone

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('✅ Hello from backend');
+});
+
+app.listen(port, () => {
+  console.log(`✅ Backend is alive on http://localhost:${port}`);
+});
